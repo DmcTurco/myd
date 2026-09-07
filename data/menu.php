8f8ce1c23df2cae84640f665be60ee5604341356
carlos pizarro <soporteinfopizarro@gmail.com>
2026-09-07 08:21:10 -0500

    quitar menu servicios seguros generales

--- data/menu.php
+++ data/menu.php
@@ -6,7 +6,7 @@
 $menu_servicios = [
     [
         'slug'   => 'soluciones-integrales',
-        'titulo' => 'SOLUCIONES INTEGRALES EN HERRAMIENTAS FINANCIERAS Y SEGUROS',
+        'titulo' => 'HERRAMIENTAS FINANCIERAS',
         'hijos'  => [
             ['slug' => 'linea-credito',        'titulo' => 'Gestión para Apertura y Aumento de Línea de Crédito para las Cartas Fianzas'],
             ['slug' => 'cartas-fianzas-obras', 'titulo' => 'Gestión de Cartas Fianzas para Obras Públicas y Privadas'],
